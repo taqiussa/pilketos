d126e40e19645e97a420c7cb9186265079a6eeaa
taqiussa <taqiussa@gmail.com>
2026-01-28 21:55:18 +0700

    s

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +19,12 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
-        //
+        if (env('APP_ENV') !== 'local') {
+            $url->forceScheme('https');
+        } else {
+            $url->forceScheme('http');
+        }
     }
 }
